d5c9867c038b319dfeb8a4e259bec9176d849443
Jan Pospíšil <jenda.pospec@gmail.com>
2023-10-01 00:23:16 +0300

    Item Holder layout

--- app/src/main/java/com/pospecstudio/todolist/ui/ItemsViewHolder.java
+++ app/src/main/java/com/pospecstudio/todolist/ui/ItemsViewHolder.java
@@ -1,29 +1,61 @@
 package com.pospecstudio.todolist.ui;
 
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.todolist.R;
 import com.pospecstudio.todolist.data.Item;
 import org.jetbrains.annotations.NotNull;
 
 public class ItemsViewHolder extends RecyclerView.ViewHolder {
-
+    private final Context context;
     private final TextView nameText;
+    private final TextView idealText;
+    private final EditText currentText;
+    private final ImageButton editButton;
+    private final GradientDrawable background;
+    private final GradientDrawable currentTextBackground;
     private Item item;
 
     public ItemsViewHolder(@NonNull @NotNull View itemView) {
         super(itemView);
+        context = itemView.getContext();
         nameText = itemView.findViewById(R.id.nameText);
+        idealText = itemView.findViewById(R.id.idealCountText);
+        currentText = itemView.findViewById(R.id.currCountText);
+        editButton = itemView.findViewById(R.id.editButton);
+        editButton.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_edit_icon));
+        ((GradientDrawable)editButton.getBackground()).setColor(ContextCompat.getColor(context, R.color.white));
+        background = (GradientDrawable) itemView.getBackground();
+        currentTextBackground = (GradientDrawable) currentText.getBackground();
     }
 
     public void bind(Item item) {
         this.item = item;
         nameText.setText(item.getItemName());
+        idealText.setText(Integer.toString(item.getIdealCount()));
+        currentText.setText(Integer.toString(item.getCurrCount()));
+        updateColors();
     }
 
-    public Item getItem() {
-        return item;
+    private void updateColors() {
+        background.setColor(FilledTypeToColor.primary(item.getFilledType(), context));
+        currentText.setText(Integer.toString(item.getCurrCount()));
+        currentTextBackground.setColor(FilledTypeToColor.secondary(item.getFilledType(), context));
+        editButton.setColorFilter(FilledTypeToColor.primary(item.getFilledType(), context), android.graphics.PorterDuff.Mode.MULTIPLY);
+    }
+
+    public void increase(View v) {
+
+    }
+
+    public void decrease(View v) {
+
     }
 }

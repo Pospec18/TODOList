d273ac41a926bc16f9924558601f9a68b9391cbd
Jan Pospíšil <jenda.pospec@gmail.com>
2024-11-23 17:08:11 +0100

    Load file unit test

--- app/src/test/java/com/pospecstudio/todolist/ExampleUnitTest.java
+++ app/src/test/java/com/pospecstudio/todolist/ExampleUnitTest.java
@@ -1,6 +1,10 @@
 package com.pospecstudio.todolist;
 
+import com.pospecstudio.todolist.data.ItemHolder;
+import com.pospecstudio.todolist.data.ItemListsHolder;
 import org.junit.Test;
+
+import java.io.*;
 
 import static org.junit.Assert.*;
 
@@ -11,7 +15,30 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void load_lists() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        assert classLoader != null;
+        InputStream inputStream = classLoader.getResourceAsStream("lists.txt");
+        assert inputStream != null;
+        try (ObjectInputStream oos = new ObjectInputStream(inputStream)) {
+            ItemListsHolder val = (ItemListsHolder) oos.readObject();
+            assertNotNull(val);
+        } catch (IOException | ClassNotFoundException ignored) {
+            assert(false);
+        }
+    }
+
+    @Test
+    public void load_list_holder() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        assert classLoader != null;
+        InputStream inputStream = classLoader.getResourceAsStream("general.txt");
+        assert inputStream != null;
+        try (ObjectInputStream oos = new ObjectInputStream(inputStream)) {
+            ItemHolder val = (ItemHolder) oos.readObject();
+            assertNotNull(val);
+        } catch (IOException | ClassNotFoundException e) {
+            assert(false);
+        }
     }
 }

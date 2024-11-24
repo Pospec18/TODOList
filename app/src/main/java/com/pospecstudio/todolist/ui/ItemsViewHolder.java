e1e01a27b04873ee348b60397c4758a47c808c07
Jan Pospíšil <jenda.pospec@gmail.com>
2024-11-24 22:16:14 +0100

    Tweek visuals

--- app/src/main/java/com/pospecstudio/todolist/ui/ItemsViewHolder.java
+++ app/src/main/java/com/pospecstudio/todolist/ui/ItemsViewHolder.java
@@ -70,7 +70,7 @@ public class ItemsViewHolder extends RecyclerView.ViewHolder {
         background.mutate();
         background.setColors(new int[] { FilledTypeToColor.primary(type, context), FilledTypeToColor.semitransparent(type, context)});
         currentText.setText(String.format(Locale.ENGLISH, "%d", item.getCurrCount()));
-        currentTextBackground.setColor(FilledTypeToColor.secondary(type, context));
+        currentTextBackground.setColor(FilledTypeToColor.primary(type, context));
         editButton.setColorFilter(FilledTypeToColor.primary(type, context), android.graphics.PorterDuff.Mode.MULTIPLY);
         indexText.setTextColor(FilledTypeToColor.darker(type, context));
     }
